898a1b9e05444a7ed9b71911c24f78e95f0e2c81
Jenny Fothergill <jennyfothergill@u.boisestate.edu>
2019-12-05 17:16:29 -0700

    better layout

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import numpy as np
 from matplotlib.backends.qt_compat import is_pyqt5 # QtCore, QtWidgets, is_pyqt5
 from PySide2 import QtCore, QtWidgets
 if is_pyqt5():
-    print("using pyqt5")
     from matplotlib.backends.backend_qt5agg import (
         FigureCanvas, NavigationToolbar2QT as NavigationToolbar)
 else:
@@ -27,13 +26,25 @@ scene = visualize(methane, show_box=True)
 class ApplicationWindow(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
-        layout = QtWidgets.QHBoxLayout()
+        self.title = "diffractometer"
+        self.initUI()
 
-        # Add the SceneView widget
-        self._view = interact.SceneView(scene)
-        layout.addWidget(self._view)
+    def initUI(self):
+        self.setWindowTitle(self.title)
 
-        self._view.c.update_camera.connect(self._update_camera)
+        self.createGridLayout()
+
+        windowlayout = QtWidgets.QVBoxLayout()
+        windowlayout.addWidget(self.horizontalGroupBox)
+
+        self._label = QtWidgets.QLabel()
+        self._label.setText(self._view.scene.camera.__repr__())
+        windowlayout.addWidget(self._label)
+
+        self.setLayout(windowlayout)
+
+        self.show()
+
 
         #dynamic_canvas = FigureCanvas(Figure(figsize=(5, 3)))
         #layout.addWidget(dynamic_canvas)
@@ -45,12 +56,25 @@ class ApplicationWindow(QtWidgets.QWidget):
         #self._dynamic_ax.plot(t, np.sin(t))
         #self._dial.valueChanged.connect(self._update_canvas)
 
-        self._label = QtWidgets.QLabel()
-        self._label.setText(str(self._view.scene.camera))
-        layout.addWidget(self._label)
         #self._dial.valueChanged.connect(self._update_text)
 
-        self.setLayout(layout)
+    def createGridLayout(self):
+        self.horizontalGroupBox = QtWidgets.QGroupBox()
+        layout = QtWidgets.QGridLayout()
+        #layout.setColumnStretch(1, 4)
+        #layout.setColumnStretch(2, 4)
+
+        # Add the SceneView widget
+        self._view = interact.SceneView(scene)
+        self._view.c.update_camera.connect(self._update_camera)
+        layout.addWidget(self._view,0,0)
+
+        # Add the diffraction widget
+        self._diffract = QtWidgets.QLabel()
+        self._diffract.setText("diffraction pattern")
+        layout.addWidget(self._diffract,0,1)
+
+        self.horizontalGroupBox.setLayout(layout)
 
     #def _update_canvas(self):
     #    self._dynamic_ax.clear()
@@ -64,7 +88,7 @@ class ApplicationWindow(QtWidgets.QWidget):
     def _update_camera(self, value):
         self._label.clear()
         # display the camera value
-        self._label.setText(str(value))
+        self._label.setText(value.__repr__())
 
 
 if __name__ == "__main__":
