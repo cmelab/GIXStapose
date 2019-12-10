ef3f680102033dc74dbe2e3adfacb08477a6c593
Jenny <jennyfothergill@u.boisestate.edu>
2019-12-10 13:31:05 -0700

    how did that get added? oops

--- to_delete/demo.py
+++
@@ -1,8 +0,0 @@
-from PySide2 import QtCore, QtGui, QtWidgets
-from power_bar import PowerBar
-
-
-app = QtWidgets.QApplication([])
-volume = PowerBar(["#5e4fa2", "#3288bd", "#66c2a5", "#abdda4", "#e6f598", "#ffffbf", "#fee08b", "#fdae61", "#f46d43", "#d53e4f", "#9e0142"])
-volume.show()
-app.exec_()
