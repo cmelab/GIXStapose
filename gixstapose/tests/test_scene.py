76c64fec394ff852780689eeceafa41bc3de834e
Jenny Fothergill <jennyfothergill@u.boisestate.edu>
2021-09-28 17:00:35 -0600

    Update tests

--- gixstapose/tests/test_scene.py
+++ gixstapose/tests/test_scene.py
@@ -1,29 +1,20 @@
+import os
+
 import fresnel
-import mbuild as mb
 
-from gixstapose.draw_scene import create_scene
+from gixstapose.draw_scene import get_scene
 
-class Methane(mb.Compound):
-    def __init__(self):
-        super(Methane, self).__init__()
-        carbon = mb.Particle(name="C")
-        self.add(carbon, label="C[$]")
 
-        hydrogen = mb.Particle(name="H", pos=[0.1, 0, -0.07])
-        self.add(hydrogen, label="HC[$]")
+test_dir = os.path.dirname(__file__)
+data_dir = os.path.join(test_dir, "../data")
 
-        self.add_bond((self[0], self["HC"][0]))
 
-        self.add(mb.Particle(name="H", pos=[-0.1, 0, -0.07]), label="HC[$]")
-        self.add(mb.Particle(name="H", pos=[0, 0.1, 0.07]), label="HC[$]")
-        self.add(mb.Particle(name="H", pos=[0, -0.1, 0.07]), label="HC[$]")
-
-        self.add_bond((self[0], self["HC"][1]))
-        self.add_bond((self[0], self["HC"][2]))
-        self.add_bond((self[0], self["HC"][3]))
-
-def test_visualise():
-    methane = Methane()
-    scene = create_scene(methane)
+def test_scene_gsd():
+    methanegsd = os.path.join(data_dir, "methane.gsd")
+    scene, info = get_scene(methanegsd)
     assert type(scene) is type(fresnel.Scene())
 
+def test_scene_comp():
+    methanemol2 = os.path.join(data_dir, "methane.mol2")
+    scene, info = get_scene(methanemol2)
+    assert type(scene) is type(fresnel.Scene())
