272cd17a22f4424543751292157273da40b01c16
Jenny Fothergill <jennyfothergill@u.boisestate.edu>
2021-09-28 16:54:38 -0600

    Clean up code and lessen dependency on mbuild
    I was finding that converting things to an mbuild compound, especially
    in the case that we already have a gsd file which is easier to work
    with, was slowing us down A LOT.
    
    - move color/radii dicts to separate file,
    - add get_gsd_info, get_comp_info so gsd files don't have to
    be converted to compounds
    - add general get_info function
    - create_scene also returns info for use by diffractometer
    - remove unecessary functions mb_box_convert, from_gsd, compound_load, and distance

--- gixstapose/draw_scene.py
+++ gixstapose/draw_scene.py
@@ -1,234 +1,89 @@
+import os
 import random
 
+import fresnel
+import gsd.hoomd
 import matplotlib.cm
 from matplotlib import colors as mplcolors
-import numpy as np
-import gsd
-import gsd.pygsd
-import gsd.hoomd
-
-import fresnel
 import mbuild as mb
+import numpy as np
 import PIL
 
-cpk_colors = {
-    "H": fresnel.color.linear([1.00, 1.00, 1.00]),  # white
-    "C": fresnel.color.linear([0.30, 0.30, 0.30]),  # grey
-    "N": fresnel.color.linear([0.13, 0.20, 1.00]),  # dark blue
-    "O": fresnel.color.linear([1.00, 0.13, 0.00]),  # red
-    "F": fresnel.color.linear([0.12, 0.94, 0.12]),  # green
-    "Cl": fresnel.color.linear([0.12, 0.94, 0.12]),  # green
-    "Br": fresnel.color.linear([0.60, 0.13, 0.00]),  # dark red
-    "I": fresnel.color.linear([0.40, 0.00, 0.73]),  # dark violet
-    "He": fresnel.color.linear([0.00, 1.00, 1.00]),  # cyan
-    "Ne": fresnel.color.linear([0.00, 1.00, 1.00]),  # cyan
-    "Ar": fresnel.color.linear([0.00, 1.00, 1.00]),  # cyan
-    "Xe": fresnel.color.linear([0.00, 1.00, 1.00]),  # cyan
-    "Kr": fresnel.color.linear([0.00, 1.00, 1.00]),  # cyan
-    "P": fresnel.color.linear([1.00, 0.60, 0.00]),  # orange
-    "S": fresnel.color.linear([1.00, 0.90, 0.13]),  # yellow
-    "B": fresnel.color.linear([1.00, 0.67, 0.47]),  # peach
-    "Li": fresnel.color.linear([0.47, 0.00, 1.00]),  # violet
-    "Na": fresnel.color.linear([0.47, 0.00, 1.00]),  # violet
-    "K": fresnel.color.linear([0.47, 0.00, 1.00]),  # violet
-    "Rb": fresnel.color.linear([0.47, 0.00, 1.00]),  # violet
-    "Cs": fresnel.color.linear([0.47, 0.00, 1.00]),  # violet
-    "Fr": fresnel.color.linear([0.47, 0.00, 1.00]),  # violet
-    "Be": fresnel.color.linear([0.00, 0.47, 0.00]),  # dark green
-    "Mg": fresnel.color.linear([0.00, 0.47, 0.00]),  # dark green
-    "Ca": fresnel.color.linear([0.00, 0.47, 0.00]),  # dark green
-    "Sr": fresnel.color.linear([0.00, 0.47, 0.00]),  # dark green
-    "Ba": fresnel.color.linear([0.00, 0.47, 0.00]),  # dark green
-    "Ra": fresnel.color.linear([0.00, 0.47, 0.00]),  # dark green
-    "Ti": fresnel.color.linear([0.60, 0.60, 0.60]),  # grey
-    "Fe": fresnel.color.linear([0.87, 0.47, 0.00]),  # dark orange
-    "default": fresnel.color.linear([0.87, 0.47, 1.00]),  # pink
-}
+from gixstapose.color_dicts import cpk_colors, bsu_colors, radii_dict
 
-bsu_colors = [
-        fresnel.color.linear([0.00, 0.20, 0.63]), # blue
-        fresnel.color.linear([0.84, 0.30, 0.04]), # orange
-        fresnel.color.linear([0.00, 0.23, 0.44]), # blue
-        fresnel.color.linear([0.00, 0.42, 0.65]), # blue
-        fresnel.color.linear([0.00, 0.45, 0.81]), # blue
-        fresnel.color.linear([0.25, 0.38, 0.60]), # blue
-        fresnel.color.linear([0.17, 0.99, 1.00]), # blue
-        fresnel.color.linear([1.00, 0.39, 0.03]), # orange
-        fresnel.color.linear([1.00, 0.40, 0.00]), # orange
-        ]
 
-# Made space to add more later
-radii_dict = {"H": 0.05, "default": 0.06}
-
-def compound_load(inputfile, frame=-1):
-    """
-    Loads an input file into an mbuild.Compound
+def get_scene(inputfile, frame=-1, color="cpk", scale=1.0, show_bonds=False):
+    """Loads an input file into a fresnel.Scene.
 
     Parameters
     ----------
     inputfile: str,
-               path to input file
+        Path to input file
     frame: int,
-           if inputfile is a trajectory, which frame to load. Supports
-           negative indexing (default -1)
+        Which frame to load if inputfile is a trajectory. Supports negative
+        indexing (default -1)
+    color : str, default "cpk"
+        Color scheme to use
+        ("cpk", "bsu", name of a matplotlib colormap, or a custom dictionary)
+    scale : float, default 1.0
+        Scaling factor for the particle radii, bond and box lengths
+    show_bonds : bool, default False
+        Whether to show bonds
 
     Returns
     -------
-    mbuild.Compound
+    fresnel.Scene
     """
-    try:
-        compound = from_gsd(inputfile, frame=frame)
-    except RuntimeError:
-        compound = mb.load(inputfile)
-    return compound
+    info = get_info(inputfile, frame, show_bonds)
+
+    return create_scene(info, color, scale, show_bonds), info
 
 
-def from_gsd(gsdfile, frame=-1, coords_only=False, scale=1.0):
-    """
-    Given a trajectory gsd file creates an mbuild.Compound.
-    If there are multiple separate molecules, they are returned
-    as one compound.
+def create_scene(info, color="cpk", scale=1.0, show_bonds=False):
+    """Create a fresnel.Scene object.
+
+    Adds geometries for particles, bonds, and box (or boundingbox).
 
     Parameters
     ----------
-    gsdfile : str, filename
-    frame : int, frame number (default -1)
-    coords_only : bool (default False)
-        If True, return compound with no bonds
-    scale : float, scaling factor multiplied to coordinates (default 1.0)
-
-    Returns
-    -------
-    mbuild.Compound
-    """
-    f = gsd.pygsd.GSDFile(open(gsdfile, 'rb'))
-    t = gsd.hoomd.HOOMDTrajectory(f)
-
-    snap = t[frame]
-    bond_array = snap.bonds.group
-    n_atoms = snap.particles.N
-
-    # Add particles
-    comp = mb.Compound()
-    comp.box = mb.Box(
-            lengths=snap.configuration.box[:3] * scale,
-            angles=snap.configuration.box[3:]
-            )
-    for i in range(n_atoms):
-        name = snap.particles.types[snap.particles.typeid[i]]
-        xyz = snap.particles.position[i] * scale
-        charge = snap.particles.charge[i]
-
-        atom = mb.Particle(name=name, pos=xyz, charge=charge)
-        comp.add(atom, label=str(i))
-
-    if not coords_only:
-        # Add bonds
-        for i in range(bond_array.shape[0]):
-            atom1 = int(bond_array[i][0])
-            atom2 = int(bond_array[i][1])
-            comp.add_bond([comp[atom1], comp[atom2]])
-    return comp
-
-def distance(pos1, pos2):
-    """
-    Calculates euclidean distance between two points.
-
-    Parameters
-    ----------
-    pos1, pos2 : ((3,) numpy.ndarray), xyz coordinates
-        (2D also works)
-
-    Returns
-    -------
-    float distance
-    """
-    return np.linalg.norm(pos1 - pos2)
-
-
-def mb_box_convert(box):
-    """
-    Convert an mbuild box object to hoomd/gsd style: [Lx, Ly, Lz, xy, xz, yz]
-    These sites are helpful as reference:
-    http://gisaxs.com/index.php/Unit_cell
-    https://hoomd-blue.readthedocs.io/en/stable/box.html
-
-    Parameters
-    ----------
-    box : mbuild.Box()
-
-    Returns
-    -------
-    numpy array
-    """
-    Lx, Ly, Lz = box.lengths
-    alpha, beta, gamma  = np.deg2rad(box.angles)
-
-    frac = np.cos(alpha) - np.cos(beta) * np.cos(gamma) / np.sin(gamma)
-    c = np.sqrt(1 - np.cos(beta) ** 2 - frac ** 2)
-
-    xy = np.cos(gamma) / np.sin(gamma)
-    xz = frac / c
-    yz = np.cos(beta) / c
-
-    return np.array([Lx, Ly, Lz, xy, xz, yz])
-
-
-def create_scene(comp, color="cpk", scale=1.0, box=None):
-    """
-    Create a fresnel.Scene object from an  mbuild Compound. Adds
-    geometries for particles, bonds, and box (or boundingbox).
-
-    Parameters
-    ----------
-    comp : (mbuild.Compound),
-           compound to visualize
-    color : ("cpk", "bsu", the name of a matplotlib colormap,
-            or a custom dictionary),
-            color scheme to use (default "cpk")
-    scale : (float),
-            scaling factor for the particle, bond, and box radii (default 1.0)
-    box : (mb.Box),
-          box object for the structure. If no box is provided,
-          comp.boundingbox is used (default None)
+    info : list
+        List containing N, types, typeids, positions, N_bonds, bonds, box
+    color : str, default "cpk"
+        Color scheme to use
+        ("cpk", "bsu", name of a matplotlib colormap, or a custom dictionary)
+    scale : float, default 1.0
+        Scaling factor for the particle radii, bond and box lengths
+    show_bonds : bool, default False
+        Whether to show bonds
 
     Returns
     -------
     fresnel.Scene
     """
 
-    ## Extract some info about the compound
-    N = comp.n_particles
-    particle_names = [p.name for p in comp.particles()]
-
-    N_bonds = comp.n_bonds
-    if N_bonds > 0:
-        # all_bonds.shape is (nbond, 2 ends, xyz)
-        all_bonds = np.stack([
-            np.stack((i[0].pos, i[1].pos)) for i in comp.bonds()
-            ])
+    N, types, typeids, positions, N_bonds, bonds, box = info
 
     color_array = np.empty((N, 3), dtype="float64")
     if type(color) is dict:
         # Populate the color_array with colors based on particle name
         # if name is not defined in the dictionary, try using the cpk dictionary
-        for i, n in enumerate(particle_names):
+        for i, n in enumerate(typeids):
             try:
-                ncolor = color[n]
+                ncolor = color[types[n]]
                 color_array[i, :] = fresnel.color.linear(
-                        mplcolors.to_rgba(ncolor)
-                        )
+                    mplcolors.to_rgba(ncolor)
+                )
             except KeyError:
                 try:
-                    color_array[i, :] = cpk_colors[n]
+                    color_array[i, :] = cpk_colors[types[n]]
                 except KeyError:
                     color_array[i, :] = cpk_colors["default"]
     elif color == "cpk":
         # Populate the color_array with colors based on particle name
         # -- if name is not defined in the dictionary, use pink (the default)
-        for i, n in enumerate(particle_names):
+        for i, n in enumerate(typeids):
             try:
-                color_array[i, :] = cpk_colors[n]
+                color_array[i, :] = cpk_colors[types[n]]
             except KeyError:
                 color_array[i, :] = cpk_colors["default"]
     elif color == "bsu":
@@ -237,11 +92,8 @@ def create_scene(comp, color="cpk", scale=1.0, box=None):
         # brand-standards/colors/
         # if there are more unique particle names than colors,
         # colors will be reused
-        unique_names = list(set(particle_names))
-        for i, n in enumerate(particle_names):
-            color_array[i, :] = bsu_colors[
-                    uniq_atoms.index(n) % len(bsu_colors)
-                    ]
+        for i, n in enumerate(typeids):
+            color_array[i, :] = bsu_colors[types.index(n) % len(bsu_colors)]
     else:
         # Populate the color_array with colors based on particle name
         # choose colors evenly distributed through a matplotlib colormap
@@ -249,29 +101,27 @@ def create_scene(comp, color="cpk", scale=1.0, box=None):
             cmap = matplotlib.cm.get_cmap(name=color)
         except ValueError:
             print(
-                "The 'color' argument takes either 'cpk', 'bsu', or the name",
-                " of a matplotlib colormap."
+                "The 'color' argument takes either 'cpk', 'bsu', or the name "
+                "of a matplotlib colormap."
             )
             raise
         mapper = matplotlib.cm.ScalarMappable(
             norm=mplcolors.Normalize(vmin=0, vmax=1, clip=True), cmap=cmap
         )
-        particle_types = list(set(particle_names))
-        N_types = len(particle_types)
+        N_types = len(types)
         v = np.linspace(0, 1, N_types)
         # Color by typeid
-        type_ids = np.array([particle_types.index(i) for i in particle_names])
         for i in range(N_types):
-            color_array[type_ids == i] = fresnel.color.linear(
-                    mapper.to_rgba(v)[i]
-                    )
+            color_array[typeids == i] = fresnel.color.linear(
+                mapper.to_rgba(v)[i]
+            )
 
     # Make an array of the radii based on particle name
     # -- if name is not defined in the dictionary, use default
     rad_array = np.empty((N), dtype="float64")
-    for i, n in enumerate(particle_names):
+    for i, n in enumerate(typeids):
         try:
-            rad_array[i] = radii_dict[n] * scale
+            rad_array[i] = radii_dict[types[n]] * scale
         except KeyError:
             rad_array[i] = radii_dict["default"] * scale
 
@@ -280,7 +130,7 @@ def create_scene(comp, color="cpk", scale=1.0, box=None):
 
     # Spheres for every particle in the system
     geometry = fresnel.geometry.Sphere(scene, N=N)
-    geometry.position[:] = comp.xyz
+    geometry.position[:] = positions
     geometry.material = fresnel.material.Material(roughness=1.0)
     geometry.outline_width = 0.01 * scale
 
@@ -292,7 +142,7 @@ def create_scene(comp, color="cpk", scale=1.0, box=None):
     geometry.radius[:] = rad_array
 
     # bonds
-    if N_bonds > 0:
+    if N_bonds > 0 and show_bonds:
         bonds = fresnel.geometry.Cylinder(scene, N=N_bonds)
         bonds.material = fresnel.material.Material(roughness=0.5)
         bonds.outline_width = 0.01 * scale
@@ -301,26 +151,81 @@ def create_scene(comp, color="cpk", scale=1.0, box=None):
         bond_colors = np.ones((N_bonds, 3), dtype="float64")
 
         bonds.material.primitive_color_mix = 1.0
-        bonds.points[:] = all_bonds
+        bonds.points[:] = bonds
 
         bonds.color[:] = np.stack(
-            [fresnel.color.linear(bond_colors),
-             fresnel.color.linear(bond_colors)
-             ], axis=1
+            [
+                fresnel.color.linear(bond_colors),
+                fresnel.color.linear(bond_colors)
+            ], axis=1
         )
         bonds.radius[:] = [0.03 * scale] * N_bonds
 
-    if box:
-        # Use comp.box, unless it does not exist, then use comp.boundingbox
-        try:
-            new_box = mb_box_convert(box)
-        except AttributeError:
-            new_box = mb_box_convert(comp.boundingbox)
-        # Create box in fresnel
-        fresnel.geometry.Box(scene, new_box, box_radius=0.008 * scale)
+    # Create box in fresnel
+    fresnel.geometry.Box(scene, box, box_radius=0.008 * scale)
 
     # Set the initial camera position
-    max_dist = np.max(comp.xyz) - np.min(comp.xyz)
+    max_dist = np.max(positions) - np.min(positions)
     scene.camera.height = 1.5 * max_dist
     scene.camera.position = [max_dist, max_dist, max_dist]
     return scene
+
+
+def get_comp_info(comp, show_bonds):
+    N = comp.n_particles
+    names = [p.name for p in comp.particles()]
+    types = list(set(names))
+    typeids = np.array([types.index(i) for i in names])
+    positions = comp.xyz
+
+    N_bonds = comp.n_bonds
+    if N_bonds > 0 and show_bonds:
+        # bonds.shape is (nbond, 2 ends, xyz)
+        bonds = np.stack(
+            [np.stack((i[0].pos, i[1].pos)) for i in comp.bonds()]
+        )
+    else:
+        bonds = None
+
+    # Use comp.box, unless it does not exist, then use comp.boundingbox
+    if comp.box is not None:
+        box = comp.box
+    else:
+        box = comp.get_boundingbox()
+    box = np.hstack([box.lengths, box.tilt_factors])
+
+    return [N, types, typeids, positions, N_bonds, bonds, box]
+
+
+def get_gsd_info(gsdfile, frame, show_bonds):
+    with gsd.hoomd.open(gsdfile) as f:
+        snap = f[frame]
+
+    N = snap.particles.N
+    types = snap.particles.types
+    typeids = snap.particles.typeid
+    positions = snap.particles.position
+
+    N_bonds = snap.bonds.N
+    if N_bonds > 0 and show_bonds:
+        # bonds.shape is (nbond, 2 ends, xyz)
+        bonds = np.stack(
+            [(positions[i],positions[j]) for (i,j) in snap.bonds.group]
+        )
+    else:
+        bonds = None
+
+    box = snap.configuration.box
+    return [N, types, typeids, positions, N_bonds, bonds, box]
+
+
+def get_info(inputfile, frame=-1, show_bonds=False):
+    name, extension = os.path.splitext(inputfile)
+    if extension == ".gsd":
+        # info contains N, types, typeids, positions, N_bonds, bonds, box
+        info = get_gsd_info(inputfile, frame, show_bonds)
+    else:
+        comp = mb.load(inputfile)
+        # info contains N, types, typeids, positions, N_bonds, bonds, box
+        info = get_comp_info(comp, show_bonds)
+    return info
