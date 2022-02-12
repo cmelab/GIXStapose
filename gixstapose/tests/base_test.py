cf3c0768944f7eac6b701bcc5dd15d6b5f8206ae
Jenny Fothergill <jennyfothergill@u.boisestate.edu>
2022-02-11 19:22:12 -0700

    Reorganize tests to add fixtures

---
+++ gixstapose/tests/base_test.py
@@ -0,0 +1,26 @@
+from pathlib import Path
+
+from gixstapose.diffractometer import camera_to_rot
+from gixstapose.draw_scene import get_info
+from gixstapose.main import camera_from_pos
+
+data_dir = str(Path(__file__).parent.parent.resolve()) + "/data/"
+
+
+class BaseTest:
+    @pytest.fixture
+    def sc10(self):
+        return data_dir + "/data/sc10.pdb"
+
+    @pytest.fixture
+    def positions_and_box(self, sc10):
+        _, _, _, positions, _, _, box = get_info(sc10)
+        return positions, box[:3]
+
+    @pytest.fixture
+    def camera100(self):
+        return camera_from_pos((1, 0, 0))
+
+    @pytest.fixture
+    def rot100(self, camera100):
+        return camera_to_rot(camera100).T
