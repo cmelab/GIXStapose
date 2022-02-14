7017c11806d80db82705d8f44caf054e3c6deb08
Jenny Fothergill <jennyfothergill@u.boisestate.edu>
2022-02-13 18:44:39 -0700

    Change info to dict and move bond queries inside show_bonds conditional

--- gixstapose/draw_scene.py
+++ gixstapose/draw_scene.py
@@ -37,7 +37,7 @@ def get_scene(
 
     Returns
     -------
-    fresnel.Scene
+    fresnel.Scene, dict
     """
     info = get_info(inputfile, frame, show_bonds)
 
@@ -53,8 +53,8 @@ def create_scene(
 
     Parameters
     ----------
-    info : list
-        List containing N, types, typeids, positions, N_bonds, bonds, box
+    info : dict
+        Dict containing N, types, typeids, positions, N_bonds, bonds, box
     color : str, default "cpk"
         Color scheme to use
         ("cpk", "bsu", name of a matplotlib colormap, or a custom dictionary)
@@ -69,29 +69,27 @@ def create_scene(
     -------
     fresnel.Scene
     """
-    N, types, typeids, positions, N_bonds, bonds, box = info
-
-    color_array = np.empty((N, 3), dtype="float64")
+    color_array = np.empty((info["N"], 3), dtype="float64")
     if type(color) is dict:
         # Populate the color_array with colors based on particle name
         # if name is not defined in the dictionary, try using the cpk dictionary
-        for i, n in enumerate(typeids):
+        for i, n in enumerate(info["typeids"]):
             try:
-                ncolor = color[types[n]]
+                ncolor = color[info["types"][n]]
                 color_array[i, :] = fresnel.color.linear(
                     mplcolors.to_rgba(ncolor)
                 )
             except KeyError:
                 try:
-                    color_array[i, :] = cpk_colors[types[n]]
+                    color_array[i, :] = cpk_colors[info["types"][n]]
                 except KeyError:
                     color_array[i, :] = cpk_colors["default"]
     elif color == "cpk":
         # Populate the color_array with colors based on particle name
         # -- if name is not defined in the dictionary, use pink (the default)
-        for i, n in enumerate(typeids):
+        for i, n in enumerate(info["typeids"]):
             try:
-                color_array[i, :] = cpk_colors[types[n]]
+                color_array[i, :] = cpk_colors[info["types"][n]]
             except KeyError:
                 color_array[i, :] = cpk_colors["default"]
     elif color == "bsu":
@@ -100,7 +98,7 @@ def create_scene(
         # brand-standards/colors/
         # if there are more unique particle names than colors,
         # colors will be reused
-        for i, n in enumerate(typeids):
+        for i, n in enumerate(info["typeids"]):
             color_array[i, :] = bsu_colors[n % len(bsu_colors)]
     else:
         # Populate the color_array with colors based on particle name
@@ -116,20 +114,20 @@ def create_scene(
         mapper = matplotlib.cm.ScalarMappable(
             norm=mplcolors.Normalize(vmin=0, vmax=1, clip=True), cmap=cmap
         )
-        N_types = len(types)
+        N_types = len(info["types"])
         v = np.linspace(0, 1, N_types)
         # Color by typeid
         for i in range(N_types):
-            color_array[typeids == i] = fresnel.color.linear(
+            color_array[info["typeids"] == i] = fresnel.color.linear(
                 mapper.to_rgba(v)[i]
             )
 
     # Make an array of the radii based on particle name
     # -- if name is not defined in the dictionary, use default
-    rad_array = np.empty((N), dtype="float64")
-    for i, n in enumerate(typeids):
+    rad_array = np.empty((info["N"]), dtype="float64")
+    for i, n in enumerate(info["typeids"]):
         try:
-            rad_array[i] = radii_dict[types[n]] * scale
+            rad_array[i] = radii_dict[info["types"][n]] * scale
         except KeyError:
             rad_array[i] = radii_dict["default"] * scale
 
@@ -140,8 +138,8 @@ def create_scene(
         scene = init_scene
 
     # Spheres for every particle in the system
-    geometry = fresnel.geometry.Sphere(scene, N=N)
-    geometry.position[:] = positions
+    geometry = fresnel.geometry.Sphere(scene, N=info["N"])
+    geometry.position[:] = info["positions"]
     geometry.material = fresnel.material.Material(roughness=1.0)
     geometry.outline_width = 0.01 * scale
 
@@ -153,16 +151,16 @@ def create_scene(
     geometry.radius[:] = rad_array
 
     # bonds
-    if N_bonds > 0 and show_bonds:
-        bond_cyls = fresnel.geometry.Cylinder(scene, N=N_bonds)
+    if info["N_bonds"] > 0 and show_bonds:
+        bond_cyls = fresnel.geometry.Cylinder(scene, N=info["N_bonds"])
         bond_cyls.material = fresnel.material.Material(roughness=0.5)
         bond_cyls.outline_width = 0.01 * scale
 
         # bonds are white
-        bond_colors = np.ones((N_bonds, 3), dtype="float64")
+        bond_colors = np.ones((info["N_bonds"], 3), dtype="float64")
 
         bond_cyls.material.primitive_color_mix = 1.0
-        bond_cyls.points[:] = bonds
+        bond_cyls.points[:] = info["bonds"]
 
         bond_cyls.color[:] = np.stack(
             [
@@ -171,14 +169,14 @@ def create_scene(
             ],
             axis=1,
         )
-        bond_cyls.radius[:] = [0.03 * scale] * N_bonds
+        bond_cyls.radius[:] = [0.03 * scale] * info["N_bonds"]
 
     if init_scene is None:
         # Create box in fresnel
-        fresnel.geometry.Box(scene, box, box_radius=0.008 * scale)
+        fresnel.geometry.Box(scene, info["box"], box_radius=0.008 * scale)
 
         # Set the initial camera position
-        max_dist = np.max(positions) - np.min(positions)
+        max_dist = np.max(info["positions"]) - np.min(info["positions"])
         scene.camera.height = 1.5 * max_dist
         scene.camera.position = [max_dist, max_dist, max_dist]
     return scene
@@ -187,7 +185,10 @@ def create_scene(
 def get_comp_info(comp, show_bonds):
     """Get info from mbuild compounds.
 
-    `info` is [N, types, typeids, positions, N_bonds, bonds, box]
+    `info` is a dictionary containing the number of particles, the particle
+    types in the system, a typeid for each particle, particle positions, the
+    number of bonds, the particle indices which are bonded, and the box
+    information. These quantities follow GSD format
 
     Parameters
     ----------
@@ -198,16 +199,7 @@ def get_comp_info(comp, show_bonds):
 
     Returns
     -------
-    [
-        int,
-        list of str,
-        array of int (N,),
-        array of float (N,3),
-        int,
-        array of int (N,2),
-        array of float (6,)
-    ]
-        [N, types, typeids, positions, N_bonds, bonds, box]
+    dict
     """
     N = comp.n_particles
     names = [p.name for p in comp.particles()]
@@ -215,11 +207,12 @@ def get_comp_info(comp, show_bonds):
     typeids = np.array([types.index(i) for i in names])
     positions = comp.xyz
 
-    N_bonds = comp.n_bonds
-    if N_bonds > 0 and show_bonds:
+    if show_bonds:
+        N_bonds = comp.n_bonds
         # bonds.shape is (nbond, 2 ends, xyz)
         bonds = np.stack([np.stack((i[0].pos, i[1].pos)) for i in comp.bonds()])
     else:
+        N_bonds = 0
         bonds = None
 
     # Use comp.box, unless it does not exist, then use comp.boundingbox
@@ -228,14 +221,25 @@ def get_comp_info(comp, show_bonds):
     else:
         box = comp.get_boundingbox()
     box = np.hstack([box.lengths, box.tilt_factors])
-
-    return [N, types, typeids, positions, N_bonds, bonds, box]
+    info = {
+        "N": N,
+        "types": types,
+        "typeids": typeids,
+        "positions": positions,
+        "N_bonds": N_bonds,
+        "bonds": bonds,
+        "box": box,
+    }
+    return info
 
 
 def get_gsd_info(gsdfile, frame, show_bonds):
     """Get info from gsd file.
 
-    `info` is [N, types, typeids, positions, N_bonds, bonds, box]
+    `info` is a dictionary containing the number of particles, the particle
+    types in the system, a typeid for each particle, particle positions, the
+    number of bonds, the particle indices which are bonded, and the box
+    information. These quantities follow GSD format
 
     Parameters
     ----------
@@ -248,16 +252,7 @@ def get_gsd_info(gsdfile, frame, show_bonds):
 
     Returns
     -------
-    [
-        int,
-        list of str,
-        array of int (N,),
-        array of float (N,3),
-        int,
-        array of int (N,2),
-        array of float (6,)
-    ]
-        [N, types, typeids, positions, N_bonds, bonds, box]
+    dict
     """
     with gsd.hoomd.open(gsdfile) as f:
         snap = f[frame]
@@ -267,23 +262,36 @@ def get_gsd_info(gsdfile, frame, show_bonds):
     typeids = snap.particles.typeid
     positions = snap.particles.position
 
-    N_bonds = snap.bonds.N
-    if N_bonds > 0 and show_bonds:
+    if show_bonds:
+        N_bonds = snap.bonds.N
         # bonds.shape is (nbond, 2 ends, xyz)
         bonds = np.stack(
             [(positions[i], positions[j]) for (i, j) in snap.bonds.group]
         )
     else:
+        N_bonds = 0
         bonds = None
 
     box = snap.configuration.box
-    return [N, types, typeids, positions, N_bonds, bonds, box]
+    info = {
+        "N": N,
+        "types": types,
+        "typeids": typeids,
+        "positions": positions,
+        "N_bonds": N_bonds,
+        "bonds": bonds,
+        "box": box,
+    }
+    return info
 
 
 def get_info(inputfile, frame=-1, show_bonds=False):
-    """Get info from inputfile.
+    """Get dictionary of information about inputfile.
 
-    `info` is [N, types, typeids, positions, N_bonds, bonds, box]
+    `info` is a dictionary containing the number of particles, the particle
+    types in the system, a typeid for each particle, particle positions, the
+    number of bonds, the particle indices which are bonded, and the box
+    information. These quantities follow GSD format
 
     Parameters
     ----------
@@ -296,16 +304,7 @@ def get_info(inputfile, frame=-1, show_bonds=False):
 
     Returns
     -------
-    [
-        int,
-        list of str,
-        array of int (N,),
-        array of float (N,3),
-        int,
-        array of int (N,2),
-        array of float (6,)
-    ]
-        [N, types, typeids, positions, N_bonds, bonds, box]
+    dict
     """
     name, extension = os.path.splitext(inputfile)
     if extension == ".gsd":
